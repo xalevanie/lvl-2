dbf7ae2e922b50c3d2d5b9fc1d6ddfb424078a3f
Madeline <Dawndylion@Dawns-iMac.local>
2017-04-30 20:14:27 -0700

    Changed a lot
    But will it work?

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 
+
 const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
+const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate() {
     const now = new Date();
-    
-    //seconds
+
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360)+  90;
-    secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     
-    //minutes
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360)+  90;
-    minHand.style.transform = 'rotate(${minsDegrees}deg)';
-    
-    //hours
-     const hour = now.getHour();
-    const hourDegrees = ((hour / 12) * 360)+  90;
-    hourHand.style.transform = 'rotate(${hourDegrees}deg)';
-    
-    //console.log(seconds);
+    const min = now.getMinutes();
+    const minDegrees = ((min / 60) * 360 + ((seconds/60)*6)) + 90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = (((hour / 12) * 360) + ((min/60)*30)) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
+setDate();
