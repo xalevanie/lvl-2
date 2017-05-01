646de61097cba85a90e69f34b640141945ba1cfb
Madeline <Dawndylion@Dawns-iMac.local>
2017-04-30 19:01:29 -0700

    hmm

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,28 @@
 // @codekit-prepend "semantic.js";
 
 
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+    
+    //seconds
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360);
+    const secondsDegrees = ((seconds / 60) * 360)+  90;
+    secondHand.style.transform = 'rotate(${secondsDegrees}deg)';
+    
+    //minutes
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360)+  90;
+    minHand.style.transform = 'rotate(${minsDegrees}deg)';
+    
+    //hours
+     const hour = now.getHour();
+    const hourDegrees = ((hour / 12) * 360)+  90;
+    hourHand.style.transform = 'rotate(${hourDegrees}deg)';
+    
     console.log(seconds);
 }
 
